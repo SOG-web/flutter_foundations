e9cdb2039a1ef9dddb5dc9efc6d7ad553c805d4e
SOG-web <raheemolalekanusman84@gmail.com>
2022-09-01 22:16:11 +0100

    change notifier provider done

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -12,15 +12,24 @@ class RandomizerPage extends StatelessWidget {
         title: const Text('Randomizer'),
       ),
       body: Center(
-          // the Text widget must be wrap in a Consumer
-          // for it to update when the state changes
-          child: Consumer<RandomizerChangeNotifier>(
-              builder: (context, notifier, child) {
-        return Text(
-          notifier.generatedNumber?.toString() ?? 'Generate a Number',
+        // the Text widget must be wrap in a Consumer
+        // for it to update when the state changes
+        //     child: Consumer<RandomizerChangeNotifier>(
+        //         builder: (context, notifier, child) {
+        //   return Text(
+        //     notifier.generatedNumber?.toString() ?? 'Generate a Number',
+        //     style: const TextStyle(fontSize: 42),
+        //   );
+        // })
+        child: Text(
+          context
+                  .watch<RandomizerChangeNotifier>()
+                  .generatedNumber
+                  ?.toString() ??
+              'Generate a Number',
           style: const TextStyle(fontSize: 42),
-        );
-      })),
+        ),
+      ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           context.read<RandomizerChangeNotifier>().generateRandomNumber();
